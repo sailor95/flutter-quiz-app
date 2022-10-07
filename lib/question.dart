7bf1cbef34baf73f07dfd93c0801066033b817de
Dave H <bugman195@gmail.com>
2022-10-07 14:00:15 +0800

    Add style

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String qText;
+  final String qText;
 
   Question(this.qText); // Constructor
 
   @override
   Widget build(BuildContext ctx) {
-    return Text(qText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        qText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
