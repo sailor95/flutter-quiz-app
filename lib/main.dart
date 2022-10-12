6b72d6265cabe3183eeee684ccdb04c92d5eb4ee
Dave H <bugman195@gmail.com>
2022-10-13 00:08:27 +0800

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,46 +14,108 @@ class MyApp extends StatefulWidget {
 
 // _ means private class
 class _MyAppState extends State<MyApp> {
-  var _questionIdx = 0;
+  final _questions = const [
+    {
+      'text': 'What\'s your favorite color?',
+      'ans': [
+        {
+          'text': 'Black',
+          'score': 10,
+        },
+        {
+          'text': 'White',
+          'score': 5,
+        },
+        {
+          'text': 'Red',
+          'score': 3,
+        },
+        {
+          'text': 'Blue',
+          'score': 7,
+        },
+      ],
+    },
+    {
+      'text': 'What\'s your favorite animal?',
+      'ans': [
+        {
+          'text': 'Dog',
+          'score': 10,
+        },
+        {
+          'text': 'Pig',
+          'score': 3,
+        },
+        {
+          'text': 'Cat',
+          'score': 2,
+        },
+        {
+          'text': 'Cow',
+          'score': 1,
+        },
+      ],
+    },
+    {
+      'text': 'What\'s your favorite person?',
+      'ans': [
+        {
+          'text': 'Dave',
+          'score': 10,
+        },
+        {
+          'text': 'David',
+          'score': 10,
+        },
+        {
+          'text': 'KT',
+          'score': 9,
+        },
+        {
+          'text': 'Kiki',
+          'score': 8,
+        },
+      ],
+    }
+  ];
 
-  void _answerQuestion() {
+  var _questionIdx = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIdx++;
+    });
+
+    if (_questionIdx < _questions.length - 1) {
+      print('We have more questions!');
+    }
+  }
+
+  void _reset() {
+    setState(() {
+      _questionIdx = 0;
+      _totalScore = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'text': 'What\'s your favorite color?',
-        'ans': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'text': 'What\'s your favorite animal?',
-        'ans': ['Dog', 'Pig', 'Cat', 'Cow'],
-      },
-      {
-        'text': 'What\'s your favorite person?',
-        'ans': ['Dave', 'David', 'Dh', 'Me'],
-      }
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My 1st App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIdx]['text'],
-            ),
-            ...(questions[_questionIdx]['ans'] as List<String>).map((a) {
-              return Answer(_answerQuestion, a);
-            }).toList(),
-          ],
-        ),
+        body: _questionIdx < _questions.length
+            ? Quiz(
+                questions: _questions,
+                questionIdx: _questionIdx,
+                answerQuestionCb: _answerQuestion,
+              )
+            : Result(_totalScore, _reset),
       ),
     );
   }
