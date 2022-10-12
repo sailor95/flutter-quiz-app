f80fc6d024a32bbf1582828b2c638f3ae1d73c40
Dave H <bugman195@gmail.com>
2022-10-13 00:07:11 +0800

    Add Result widget

---
+++ lib/result.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int score;
+  final VoidCallback restart;
+
+  Result(this.score, this.restart);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (score <= 20) {
+      resultText = 'You are lame...';
+    } else if (score <= 25) {
+      resultText = 'You are okay~';
+    } else {
+      resultText = 'You are a great person!';
+    }
+
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          ElevatedButton(onPressed: restart, child: Text('Restart Quiz!'))
+        ],
+      ),
+    );
+  }
+}
